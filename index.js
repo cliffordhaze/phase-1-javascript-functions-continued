a3a12d59e25f211bca7d0ceeadf5476725fed22c
Nicole Gomez <nicolegomezx3@gmail.com>
2023-03-31 05:27:43 -0400

    completed javascript functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(doWork = `go to the office`) {
+    return `This Monday, I will ${doWork}.`
+}
+
+function wrapAdjective(wrapping = '*') {
+    return function(value = "special") {
+        return `You are ${wrapping}${value}${wrapping}!`
+    }
+}
